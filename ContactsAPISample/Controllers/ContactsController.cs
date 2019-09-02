17564470e67c1552018addf231d2af74912f0fbe
Jay Asbury <vbjay.net@gmail.com>
2019-09-02 19:18:06 -0400

    Update email addresses when saving a contact

--- ContactsAPISample/Controllers/ContactsController.cs
+++ ContactsAPISample/Controllers/ContactsController.cs
@@ -66,6 +66,12 @@ namespace ContactsAPISample.Controllers
             }
 
             _context.Entry(contact).State = EntityState.Modified;
+            if (!ProcessEmailAddresses(contact))
+            {
+                ModelState.AddModelError(nameof(contact.EmailAddresses), "EmailAddresses were not valid.  An email address is already being used by another contact.");
+                return BadRequest(ModelState);
+            }
+
 
             try
             {
@@ -94,8 +100,14 @@ namespace ContactsAPISample.Controllers
             {
                 return BadRequest(ModelState);
             }
+            if (!ProcessEmailAddresses(contact))
+            {
+                ModelState.AddModelError(nameof(contact.EmailAddresses), "EmailAddresses were not valid.  An email address is already being used by another contact.");
+                return BadRequest(ModelState);
+            }
 
             _context.Contacts.Add(contact);
+
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetContact", new { id = contact.ID }, contact);
@@ -120,6 +132,49 @@ namespace ContactsAPISample.Controllers
             await _context.SaveChangesAsync();
 
             return Ok(contact);
+        }
+
+        /// <summary>
+        /// Figures out how to update the email addresses
+        /// </summary>
+        /// <param name="contact"></param>
+        /// <returns></returns>
+        bool ProcessEmailAddresses(Contact contact)
+        {
+            var inDB = (from db in _context.EmailAddresses.AsNoTracking() // used to force db access and not entities that are in the context's cache
+                        where contact.EmailAddresses.Select(e => e.Email).Contains(db.Email) || contact.ID==db.ContactID
+                        select db).ToList();
+            if (inDB.Any() && inDB.Select(e => e.ContactID).Distinct().Any(id => id != contact.ID))
+            {
+                return false;
+            }
+
+
+            var matched = (from db in inDB
+                           join updated in contact.EmailAddresses ?? new List<EmailAddress>() on db.Email equals updated.Email
+                           select new { FromDB = db, Current = updated }).ToList();
+
+            var toUpdate = matched;
+            var toRemove = inDB.Except(matched.Select(m => m.FromDB));
+            var toAdd = contact.EmailAddresses.Except(matched.Select(m => m.Current));
+
+
+            foreach (var address in toUpdate)
+            {
+                _context.Entry(address.FromDB).State = EntityState.Detached;
+                _context.EmailAddresses.Attach(address.Current);
+                _context.Entry(address.Current).State = EntityState.Modified;
+
+            }
+            foreach (var address in toAdd)
+            {
+                _context.EmailAddresses.Add(address);
+            }
+            foreach (var address in toRemove)
+            {
+                _context.EmailAddresses.Remove(address);
+            }
+            return true;
         }
 
         private bool ContactExists(long id)
